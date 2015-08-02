37f1a49fd54dcd6f987f37221935f9d962df5a13
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-08-02 11:25:49 +0800

    Create world-project.js

---
+++ world-project.js
@@ -0,0 +1,10 @@
+/*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* World                                                                       *
+* By Scarlet and Operation-World members (https://github.com/Operation-World) *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*/
+
+function AI() {
+ inp = document.getElementsByTagName('input')[0];
+}
